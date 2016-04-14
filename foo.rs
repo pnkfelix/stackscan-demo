52e452ba4b0cacd04395bca33ba783844e77d22f
Felix S. Klock II <pnkfelix@pnkfx.org>
2016-04-14 14:55:49 +0200

    hook in backtrace hack module.
    next step: figure out the correspondance between the data in the
    stackmap and the data in the backtrace.

--- foo.rs
+++ foo.rs
@@ -2,7 +2,10 @@
 #![feature(question_mark)]
 #![feature(type_ascription)]
 #![feature(rustc_attrs)]
+#![feature(libc)]
+#![feature(const_fn)]
 #![allow(unused_features)]
+extern crate libc;
 
 use std::intrinsics;
 
@@ -11,6 +14,7 @@ mod byteorder;
 #[allow(dead_code, unused_imports, unused_variables, unused_mut)]
 mod elf;
 
+mod backtrace_hack;
 mod llvm_stackmaps;
 
 fn subcall(data: *mut u8) {
@@ -32,7 +36,12 @@ fn subcall(data: *mut u8) {
     println!("Finis `subcall`");
 }
 
+static mut MAIN_LOCAL: usize = 0;
+pub fn main_height() -> usize { unsafe { MAIN_LOCAL } }
+
 fn main() {
+    let local: u32 = 0;
+    unsafe { MAIN_LOCAL = &local as *const _ as usize; }
     demo().unwrap();
 }
 
@@ -49,6 +58,7 @@ impl std::convert::From<elf::ParseError> for DemoError {
 #[rustc_mir(borrowck_graphviz_postflow="/tmp/foo.dot")]
 fn demo() -> Result<(), DemoError> {
     use std::path::Path;
+    use libc::{c_char, c_int, c_void};
     use self::llvm_stackmaps::StackMap;
     use self::byteorder::LittleEndian;
 
@@ -76,6 +86,16 @@ fn demo() -> Result<(), DemoError> {
     println!("path addr: {:?}", &path as *const _);
     println!("file addr: {:?}", &file as *const _);
     println!("data addr: {:?}", &data as *const _);
+
+    println!("conservative stack_height: {}", backtrace_hack::stack_height());
+    let addresses = backtrace_hack::backtrace_return_addresses();
+    println!("backtrace:      {:?}", addresses);
+    println!("backtrace map*: {:?}", {
+        let derefed: Vec<_> = addresses.iter()
+            .map(|p| unsafe { *(*p as *const *mut c_void) })
+            .collect();
+        derefed
+    });
     unsafe {
         intrinsics::stackmap_call(0, 13, subcall, data.as_mut_ptr());
     }

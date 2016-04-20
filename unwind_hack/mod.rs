c8f88b5a840fb09973bdc8253716a9219c7b57f1
Felix S. Klock II <pnkfelix@pnkfx.org>
2016-04-20 15:37:56 +0200

    uncommented a bunch of other `extern fn` declarations from `libunwind`.
    Added a couple more small definitions that they need.

--- unwind_hack/mod.rs
+++ unwind_hack/mod.rs
@@ -1,3 +1,5 @@
+use libc::{c_char, c_double, c_int, size_t};
+
 #[cfg(target_arch="x86_64")]
 #[path="ucontext_x86_64.rs"]
 mod ucontext;
@@ -25,15 +27,19 @@ pub struct unw_cursor_t {
 }
 
 type unw_context_t = ucontext::ucontext_t;
+type unw_regnum_t = c_int;
+
+#[cfg(target_arch="x86_64")]
+type unw_fpreg_t = c_double;
 
 extern {
     fn unw_getcontext(ucp: *mut unw_context_t);
     fn unw_init_local(c: *mut unw_cursor_t, ctxt: *mut unw_context_t);
     // fn unw_init_remote(c: *mut unw_cursor_t, as: unw_addr_space_t, arg: *mut c_void);
-    // fn unw_step(cp: *mut unw_cursor_t);
-    // fn unw_get_reg(cp: *mut unw_cursor_t, reg: unw_regnum_t, valp: *mut unw_word_t);
-    // fn unw_get_fpreg(cp: *mut unw_cursor_t, reg: unw_regnum_t, valp: *mut unw_fpreg_t);
-    // fn unw_resume(cp: *mut unw_cursor_t);
+    fn unw_step(cp: *mut unw_cursor_t);
+    fn unw_get_reg(cp: *mut unw_cursor_t, reg: unw_regnum_t, valp: *mut unw_word_t);
+    fn unw_get_fpreg(cp: *mut unw_cursor_t, reg: unw_regnum_t, valp: *mut unw_fpreg_t);
+    fn unw_resume(cp: *mut unw_cursor_t);
 
     // fn unw_create_addr_space(ap: *mut unw_accessors_t, byteorder: c_int);
     // fn unw_destroy_addr_space(as: unw_addr_space_t);
@@ -41,12 +47,12 @@ extern {
     // fn unw_flush_cache(as: unw_addr_space, lo: unw_word_t, hi: unw_word_t);
     // fn unw_set_caching_policy(as: unw_addr_space, policy: unw_caching_policy_t) -> c_int;
 
-    // fn unw_regname(regnum: unw_regnum_t) -> *const c_char;
+    fn unw_regname(regnum: unw_regnum_t) -> *const c_char;
     // fn unw_get_proc_info(cp: *mut unw_cursor_t, pip: *mut unw_proc_info_t);
     // fn unw_get_save_loc(cp: *mut unw_cursor_t, _int: c_int, loc: *mut unw_save_loc_t);
-    // fn unw_is_fpreg(r: unw_regnum_t) -> c_int;
-    // fn unw_is_signal_frame(cp: *mut unw_cursor_t) -> c_int;
-    // fn unw_get_proc_name(cp: *mut unw_cursor_t, bufp: *mut c_char, len: size_t, offp: *mut unw_word_t) -> c_int;
+    fn unw_is_fpreg(r: unw_regnum_t) -> c_int;
+    fn unw_is_signal_frame(cp: *mut unw_cursor_t) -> c_int;
+    fn unw_get_proc_name(cp: *mut unw_cursor_t, bufp: *mut c_char, len: size_t, offp: *mut unw_word_t) -> c_int;
 
     // fn _U_dyn_register(di: *mut unw_dyn_info_t);
     // fn _U_dyn_cancel(di: *mut unw_dyn_info_t);
